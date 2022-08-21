a0242a2bb91420c44c8308760f85a962a173ca01
mina39khalid <minakhalid2021@gmail.com>
2022-08-21 04:04:46 +0300

    mina

---
+++ lib/explore.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
+class explore extends StatefulWidget {
+  const explore({Key? key}) : super(key: key);
+
+  @override
+  State<explore> createState() => _exploreState();
+}
+
+class _exploreState extends State<explore> {
+  var _selectedTab = _SelectedTab.home;
+
+  void _handleIndexChanged(int i) {
+    setState(() {
+      _selectedTab = _SelectedTab.values[i];
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      extendBody: true,
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            Container(
+              height: 35, width: 460,
+              margin: EdgeInsets.only(top: 10,right: 6,left: 6,bottom: 8),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius:  BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [ Padding(padding: EdgeInsets.all(3)),
+                Icon(Icons.search,color: Colors.grey[600],),
+                SizedBox(width: 4,),
+                Text('Search',style: TextStyle(color: Colors.grey[600],fontSize: 16),),
+              ],),
+            ),
+        Container(
+          width:500, height: 578,
+            color: Colors.grey.shade300,
+          child:
+            GridView.count(crossAxisCount: 3,
+              crossAxisSpacing: 3,
+              mainAxisSpacing: 3,
+              children: [
+                Post('https://images.pexels.com/photos/3997389/pexels-photo-3997389.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/965731/pexels-photo-965731.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/848205/pexels-photo-848205.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://i0.wp.com/www.highongloss.com/wp-content/uploads/2021/07/must-have-lipstick.jpg?fit=960%2C640&ssl=1'),
+                Post('https://images.pexels.com/photos/325527/pexels-photo-325527.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/1078983/pexels-photo-1078983.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/1199960/pexels-photo-1199960.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/3608263/pexels-photo-3608263.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/2547462/pexels-photo-2547462.jpeg?auto=compress&cs=tinysrgb&w=600'),
+                Post('https://images.pexels.com/photos/3861976/pexels-photo-3861976.jpeg?auto=compress&cs=tinysrgb&w=600'),
+              ],), )],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(bottom: 10),
+
+        child: DotNavigationBar(
+
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          dotIndicatorColor: Colors.white,
+          unselectedItemColor: Colors.grey[300],
+          // enableFloatingNavBar: false,
+          onTap: _handleIndexChanged,
+          items: [
+            /// Home
+            DotNavigationBarItem(
+              icon: Icon(Icons.home,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+            /// Home
+            DotNavigationBarItem(
+              icon: Icon(Icons.search,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Likes
+            DotNavigationBarItem(
+              icon: Icon(Icons.send_time_extension_outlined,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Search
+            DotNavigationBarItem(
+              icon: Icon(Icons.favorite_outline_sharp,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Profile
+            DotNavigationBarItem(
+              icon: Icon(Icons.person,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+Container Post( String url)
+{ return    Container(width: 20, height: 20,
+  decoration: BoxDecoration(
+    color: Colors.grey,
+    image: DecorationImage(
+        image: NetworkImage( url ),
+        fit: BoxFit.cover
+    ),),
+);
+}
+}
+enum _SelectedTab { home,search ,explor ,favorite, person }
+
+
+
+
---
+++ lib/instagram.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:instgram/explore.dart';
+import 'package:instgram/profile.dart';
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
+class instgram extends StatefulWidget {
+  const instgram({Key? key}) : super(key: key);
+
+  @override
+  State<instgram> createState() => _instaState();
+}
+
+class _instaState extends State<instgram> {
+  var _selectedTab = _SelectedTab.home;
+
+  void _handleIndexChanged(int i) {
+    setState(() {
+      _selectedTab = _SelectedTab.values[i];
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar:   AppBar(
+        backgroundColor: Colors.white,
+        title: Image.asset('screen/img_1.png',width: 110,height: 110,),
+        actions: [
+          Icon(Icons.add_sharp,color: Colors.black,),
+          SizedBox(width: 10,),
+          Icon(Icons.chat_rounded,color: Colors.black,),
+        ], elevation: 0,),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child:SafeArea(
+          child: Column(
+            children: [
+              SizedBox(height: 10,),
+
+
+              Container( height: 80,
+                margin: EdgeInsets.only(left: 0),
+
+                child: ListView(scrollDirection: Axis.horizontal,
+                  children: [
+                    Row(children: [
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                      Stoy(),
+                    ],
+                    )
+                  ],
+                ),
+              ),
+              Row(children: [
+                SizedBox(width: 25,),
+                Text('lsaiah'),
+                SizedBox(width: 50,),
+                Text('Jayden'),
+                SizedBox(width: 50,),
+                Text('hunter'),
+                SizedBox(width: 53,),
+                Text('Ethel'),
+                SizedBox(width: 55,),
+                Text('jake'),
+                SizedBox(width: 55,),
+                Text('jor'),
+
+              ],
+              ),
+              Divider( height: 10,color: Colors.grey,),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=> profile()
+                      )
+                      );
+                    },
+                    child: Container(width: 30,height: 30,
+                      margin: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(
+                            width: 2,color: Colors.pink
+                        ),
+                        image: new DecorationImage(
+                            image: AssetImage('screen/img_3.png') ),),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(height: 18,width: 12,),
+                      Text('microverseinc',style: TextStyle(fontSize: 15),),
+                      SizedBox(height: 20,), ],
+                  ),SizedBox(width: 344,),
+                  Icon(Icons.more_horiz_outlined), ],
+              ),
+              Image(image: AssetImage('screen/img_2.png'),width: 600,height: 400,fit: BoxFit.fitWidth,),
+              Row(children: [ SizedBox(height: 40,width: 10,),
+                Icon(Icons.favorite_border_outlined),
+                SizedBox(height: 40,width: 10,),
+                Icon(Icons.mode_comment_outlined),
+                SizedBox(height: 40,width: 13,),
+                Icon(Icons.send),
+                SizedBox(height: 40,width: 360,),
+                Icon(Icons.bookmark_border,)],
+              ),
+              Row(children: [ SizedBox(width: 10,),Text('54 likes'),],),
+              Row(children: [ SizedBox(width: 10,height: 20,), Text('microverseinc ',style:TextStyle(fontWeight: FontWeight.bold),),
+                Text('Do you want to know how to ...'), Text('more',style: TextStyle(color: Colors.grey),)],),
+              Row(children: [ SizedBox(width: 10,height: 20,), Text('View all 5 comment',style: TextStyle(color: Colors.grey),)],),
+              Row(children: [ SizedBox(width: 10), Text('july 25',style: TextStyle(color: Colors.grey,fontSize: 12),)],),
+
+
+              Column(children: [
+                GestureDetector(
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=> explore()
+                    )
+                    );
+                  },
+
+               child: Container(width: 100,height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[300],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(child: Text('Expoler',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),))],)
+
+            ],
+          ),
+
+        ),),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(bottom: 10),
+
+        child: DotNavigationBar(
+
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          dotIndicatorColor: Colors.white,
+          unselectedItemColor: Colors.grey[300],
+          // enableFloatingNavBar: false,
+          onTap: _handleIndexChanged,
+          items: [
+            /// Home
+            DotNavigationBarItem(
+              icon: Icon(Icons.home,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+            /// Home
+            DotNavigationBarItem(
+              icon: Icon(Icons.search,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Likes
+            DotNavigationBarItem(
+              icon: Icon(Icons.send_time_extension_outlined,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Search
+            DotNavigationBarItem(
+              icon: Icon(Icons.favorite_outline_sharp,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+
+            /// Profile
+            DotNavigationBarItem(
+
+              icon: Icon(Icons.person,color: Colors.black,),
+              selectedColor: Color(0xff73544C),
+            ),
+          ],
+        ),
+      ),  );
+  }
+  Container Stoy(){
+    return Container(width: 80,height: 80,
+      margin: EdgeInsets.only(right: 5,left: 5,bottom: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        border: Border.all(
+            width: 2,color: Colors.pink
+        ),
+        image: new DecorationImage(
+            image: AssetImage('screen/img.png') ),),
+    );
+
+  }
+}
+enum _SelectedTab { home,search ,explor ,favorite, person }
